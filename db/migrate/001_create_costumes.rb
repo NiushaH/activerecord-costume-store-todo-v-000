bee0fed30591882116b7f246ebb56628d8d6ec73
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 19:16:39 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -9,7 +9,14 @@
 # value types according to the spec
 
 
-class Costume < ActiveRecord::Base
-
-
+class CreateCostumes < ActiveRecord::Migration[4.2]
+  def change 
+    create_table :costumes do |t|
+      t.string :name
+      t.float :price
+      t.string :image_url
+      t.string :size
+      t.timestamps
+    end
+  end
 end
