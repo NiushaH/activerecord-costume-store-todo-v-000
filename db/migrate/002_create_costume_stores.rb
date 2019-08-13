59549315993aad9a5b89d8088109b69612c71273
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 19:19:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -2,11 +2,6 @@
 class CostumeStore < ActiveRecord::Migration[4.2]
   def change 
     create_table :costumes do |t|
-      t.string :name
-      t.float :price
-      t.string :image_url
-      t.string :size
-      t.timestamps
     end
   end
 end
