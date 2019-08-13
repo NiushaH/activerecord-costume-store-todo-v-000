8a78d3316681ea7137be66b4b4ddc012dda85da0
Niusha H <niusha.hadziomerovic@gmail.com>
2019-08-13 19:20:19 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,7 +1,7 @@
 # Create your costume_stores migration here
 class CostumeStore < ActiveRecord::Migration[4.2]
   def change 
-    create_table :costumes do |t|
+    create_table :costume_store do |t|
       t.string :name
 
     end
